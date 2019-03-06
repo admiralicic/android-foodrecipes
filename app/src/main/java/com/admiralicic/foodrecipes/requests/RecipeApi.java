2b490c1c3a96ba76dc1aedbef02b8228bfac0af4
Admir Alicic <admir.alicic@gmail.com>
2019-03-06 22:31:45 +0100

    Test api calls

---
+++ app/src/main/java/com/admiralicic/foodrecipes/requests/RecipeApi.java
@@ -0,0 +1,24 @@
+package com.admiralicic.foodrecipes.requests;
+
+import com.admiralicic.foodrecipes.requests.responses.RecipeResponse;
+import com.admiralicic.foodrecipes.requests.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RecipeApi {
+    //SEARCH
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_key
+    );
+}
---
+++ app/src/main/java/com/admiralicic/foodrecipes/util/Constants.java
@@ -0,0 +1,6 @@
+package com.admiralicic.foodrecipes.util;
+
+public class Constants {
+    public static final String BASE_URL = "https://www.food2fork.com";
+    public static final String API_KEY = "eb7c2e32f2b119a2a99998f54f339377";
+}
